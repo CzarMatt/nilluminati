c212978d4a3ea1a2be2e74234c4726ee852b59d3
Matt Mossman <mmossman@crowdcompass.com>
2016-04-11 09:53:32 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Critical Sprint
+
+This Slack Bot will appear and remind you just how dire your contributions are in this critical sprint.
+
+## Add it on your Slack channel in three steps!
+### Step 1 - Create an Outgoing Webhook integration
+You'll need to go to https://my.slack.com/services/new/outgoing-webhook and add a new Outgoing Webhook integration. 
+
+Select a channel where you want Critical Sprint to hang out. I'm using #general, for example. Additionaly, there is a Token on the Integration Settings section. Copy that value. You'll need it later.
+
+Keep this browser tab open, because we will come back to it later.
+
+![Outgoing Webhook Token](http://i.imgur.com/tn6Kfgzl.png)
+
+### Deploy this app to Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/czarmatt/critical-sprint/tree/master)
+
+Use that button to deploy this app to Heroku. Before you deploy the app, add the Outgoing Webhook Token for the config variable. Also, give your app a name.
+
+![Outgoing Webhook Token config variable](http://i.imgur.com/iIcRr5yl.png)
+
+### Add the app's URL on the Outgoing Webhook Integration.
+Your app should have a URL. If you don't know it, you can find it on Heroku under your app's settings. Take note of it.
+
+Now go back to the integration settings. Add the URL. Save your settings.
---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// vars this app uses to know who has chatted recently.
+var prevUserName = '';
+var userName = 'Chicxulub';
+
+// Need this to get user's ip address correctly.
+app.enable('trust proxy');
+
+// body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// the bot will interrupt all post requests to this app.
+app.post('*', function critical(req, res) {
+    var data = req.body;
+    var textToParse = req.body.text;
+    
+    // Protect against unknown sources by checking the token.  401 = unauthorized
+    if (process.env.OUTGOING_WEBHOOK_TOKEN && data.token !== process.env.OUTGOING_WEBHOOK_TOKEN) {
+        return res.status(401).end();
+    }
+
+    // return if slackbot is chatting because we don’t want an infinite loop
+    // slack can ban this app for spamming the chatroom.
+    if (data.user_name === 'slackbot') {
+        return res.status(200).end();
+    }
+
+    // return if the incoming user is the same as the previously stored user
+    if (data.user_name === userName) {
+        return res.status(200).end();
+    }
+
+    prevUserName = req.body.user_name === userName ? prevUserName : userName;
+    userName = req.body.user_name;
+
+    // sample strings to compare against
+    var substrings = ['wfh', 'wah', 'work at home', 'working at home', 'be in by', 'in the office by',
+        'work from home', 'working from home', 'leaving early', 'vacation', 'pto'];
+
+    // result will be true if any of the sample strings are contained within the incoming text
+    var result = containsAny(textToParse.toUpperCase(), substrings);
+
+    if (result) {
+        // we have a match!  send the bot's response
+        var botResponse = {
+            icon_url: 'http://i.imgur.com/eGBcBrA.png',
+            username: ‘Nilluminati Bot',
+            text: ' ' + userName + ', IN THE MIDDLE OF THIS CRITICAL SPRINT!!??!!?!!1'
+        };
+
+        // log out some stuff
+        console.log('response = ' + textToParse);
+        res.json(botResponse);
+
+    } else {
+        //reply to the server and don't keep it hanging. Slack might disable this bot if too many timeouts
+        console.log('Nothing to parse. ::: textToParse = ' + textToParse);
+        return res.status(200).end();
+    }
+});
+
+// the bot also replies to everyone else doing a get request.  (for testing)
+app.get('*', function(req, res) {
+    var ip = req.ip;
+    res.status(200).send(' ' + ip + ', IN THE MIDDLE OF THIS CRITICAL SPRINT!!??!!?!!1');
+});
+
+// error handler
+app.use(function(err, req, res) {
+    console.error(err.stack);
+    res.status(400).send(err.message);
+});
+
+app.listen(port, function() {
+    console.log(‘Critical Sprint Bot is listening on port ' + port);
+});
+
+function containsAny(str, substrings) {
+    for (var i = 0; i != substrings.length; i++) {
+        var substring = substrings[i].toUpperCase();
+        if (str.indexOf(substring) != -1) {
+            return substring;
+        }
+    }
+    return null;
+}
